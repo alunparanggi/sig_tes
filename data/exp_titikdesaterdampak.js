97c48a00d7f6de345f2b9f4a112178c770e8458c
alunparanggi <alun.paranggi@gmail.com>
2020-05-12 13:11:19 +0700

    tes

---
+++ data/exp_titikdesaterdampak.js
@@ -0,0 +1,9 @@
+var exp_titikdesaterdampak = {
+"type": "FeatureCollection",
+"name": "exp_titikdesaterdampak",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ogc_fid": 1, "nama": "desa canti", "kategori": "terdampak tsunami", "korban": "3", "foto": null }, "geometry": { "type": "Point", "coordinates": [ 105.622113291933672, -5.822605055962829 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": 2, "nama": "desa waymuli", "kategori": "terdampak tsunami", "korban": "3", "foto": null }, "geometry": { "type": "Point", "coordinates": [ 105.649190407181038, -5.820925353025934 ] } }
+]
+}
